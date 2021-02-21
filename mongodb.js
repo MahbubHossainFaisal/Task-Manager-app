c5d45f590e4f2d1021af3c22e80af39bb90032bb
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-21 23:56:02 +0600

    Only the database connection code

--- mongodb.js
+++ mongodb.js
@@ -5,11 +5,7 @@
 const {MongoClient,ObjectID}= require('mongodb')
 
 const id = new ObjectID()
-console.log(id);
-console.log(id.id);
-console.log(id.id.length);
-console.log(id.toHexString().length)
-console.log(id.getTimestamp())
+
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -22,35 +18,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => 
 
     const db = client.db(databaseName)
 
-    //Insert one user
-    // db.collection('users').insertOne({
-    //     _id: id, //providing my own created objectID
-    //     name: 'Shamim Hasan',
-    //     age: 22
-    // }, (error,result) => {
-    //     if(error){
-    //        return console.log('Unable to insert user!')
-    //     }
-    //     console.log(result.ops)
-    // })
-
-
-    //Practice of many insert
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Problem solving',
-    //         completed: true
-    //     }, {
-    //         description: 'Web development',
-    //         completed: false
-    //     },  {
-    //         description: 'Quiz preparation',
-    //         completed: true
-    //     }
-    // ], (error,result) => {
-    //     if(error){
-    //         return console.log('Unable to insert!')
-    //     }
-    //     console.log(result.ops)
-    // })
 })
