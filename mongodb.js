0af94f1cd8bb9a9573052d5d4a79c02ee7e9fae6
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-22 00:21:34 +0600

    read operation or query from MongoDB database using findone and find method

--- mongodb.js
+++ mongodb.js
@@ -17,5 +17,43 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => 
     }
 
     const db = client.db(databaseName)
+    //to find anything with object property 
+    db.collection('users').findOne( { name: 'Faisal'}, (error,result) => {
+        if(error){
+           return console.log('Unable to fetch!')
+        }
+        console.log(result);
+    })
 
+    //to find anything with object id 
+    db.collection('users').findOne( { _id: new ObjectID("602eb69d84599d2738a310de")}, (error,result) => {
+        if(error){
+           return console.log('Unable to fetch!')
+        }
+        console.log(result);
+    })
+
+    //to find multiple documents
+     db.collection('users').find( { name: 'Faisal'}).toArray((error,result) => {
+          if(error){
+           return console.log('Unable to fetch!')
+        }
+        console.log(result);
+     })
+     //to find the number of multiple documents 
+     db.collection('users').find( { name: 'Faisal'}).count((error,result) => {
+          if(error){
+           return console.log('Unable to fetch!')
+        }
+        console.log(result);
+     })
+
+     //another example of finding multiple documents
+     db.collection('tasks').find({ completed: false}).toArray((error,result) =>{
+
+        if(error){
+           return console.log('Unable to fetch!')
+        }
+        console.log(result);
+     })
 })
