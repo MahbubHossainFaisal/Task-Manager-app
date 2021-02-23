8ffe63439d77ac11e5d104ca2bfa7c2800e51c22
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-24 01:30:19 +0600

    updateOne and updateMany method with promises in MongoDB

--- mongodb.js
+++ mongodb.js
@@ -18,42 +18,37 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error,client) => 
 
     const db = client.db(databaseName)
     //to find anything with object property 
-    db.collection('users').findOne( { name: 'Faisal'}, (error,result) => {
-        if(error){
-           return console.log('Unable to fetch!')
-        }
-        console.log(result);
+   //  db.collection('users').findOne( { name: 'Faisal'}, (error,result) => {
+   //      if(error){
+   //         return console.log('Unable to fetch!')
+   //      }
+   //      console.log(result);
+   //  })
+
+    //Update method ,This time with promises,not with callback function
+
+   //  db.collection('users').updateOne( {_id: new ObjectID("602eb2e1d23aa234080492a7")} , {
+   //     $inc :{
+   //       age: -1
+   //     },
+   //     $set : {
+   //       name:'Shakil'
+   //     }
+   //  }).then((result) =>{
+   //     console.log(result);
+   //  }).catch((error) =>{
+   //     console.log(error)
+   //  })
+
+    db.collection('tasks').updateMany({
+       completed: false //find all the properties of completed which are false
+    },{
+      $set: {
+          completed: true //update them with true
+      }
+    }).then((result) =>{
+       console.log(result.modifiedCount)
+    }).catch((error) =>{
+       console.log(error)
     })
-
-    //to find anything with object id 
-    db.collection('users').findOne( { _id: new ObjectID("602eb69d84599d2738a310de")}, (error,result) => {
-        if(error){
-           return console.log('Unable to fetch!')
-        }
-        console.log(result);
-    })
-
-    //to find multiple documents
-     db.collection('users').find( { name: 'Faisal'}).toArray((error,result) => {
-          if(error){
-           return console.log('Unable to fetch!')
-        }
-        console.log(result);
-     })
-     //to find the number of multiple documents 
-     db.collection('users').find( { name: 'Faisal'}).count((error,result) => {
-          if(error){
-           return console.log('Unable to fetch!')
-        }
-        console.log(result);
-     })
-
-     //another example of finding multiple documents
-     db.collection('tasks').find({ completed: false}).toArray((error,result) =>{
-
-        if(error){
-           return console.log('Unable to fetch!')
-        }
-        console.log(result);
-     })
 })
