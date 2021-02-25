22bc837ab6f65ad85183cb0c4f13e98a24e9ba35
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-26 01:20:23 +0600

    another collection example with mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,22 +6,42 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name : {
+// const User = mongoose.model('User', {
+//     name : {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+// const me = new User({
+//     name: 'Faisal',
+//     age: 23
+// })
+
+// me.save().then(()=>{
+//     console.log(me)
+// }).catch((error) => {
+//     console.log("Error!",error)
+// })
+
+const Tasks = mongoose.model('Tasks', {
+    description:{
         type: String
     },
-    age: {
-        type: Number
+    completed: {
+        type: Boolean
     }
 })
 
-const me = new User({
-    name: 'Faisal',
-    age: 23
+const me = new Tasks({
+    description: 'Problem Solving',
+    completed: false
 })
 
 me.save().then(()=>{
     console.log(me)
-}).catch((error) => {
+}).catch((error)=>{
     console.log("Error!",error)
 })
